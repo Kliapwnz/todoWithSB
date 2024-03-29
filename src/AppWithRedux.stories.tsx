140f850f5823d93cbf0df47d00fe6bb2dd789889
Evgeniy Klimovch <kliasanik@gmail.com>
2024-03-30 01:28:08 +0300

    ReduxStoreProviderDecorator work in AppWithReduxBaseExample

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import {AppWithRedux} from "./AppWithRedux";
 import {Provider} from "react-redux";
 import {store} from "./state/store";
+import {ReduxStoreProviderDecorator} from "./stories/ReduxStoreProviderDecorator";
 
 export default {
    title: "AppWithRedux Component",
-   component: AppWithRedux
+   component: AppWithRedux,
+   decorators: [ReduxStoreProviderDecorator]
 }
 
 export const AppWithReduxBaseExample = () => {
-   return <Provider store={store}> <AppWithRedux/></Provider>
+   return <AppWithRedux/>
 }
--- src/stories/ReduxStoreProviderDecorator.tsx
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -1,3 +1,6 @@
+import {Provider} from "react-redux";
+import {store} from "../state/store";
+
 export const ReduxStoreProviderDecorator = (story: any) => {
-   return story();
+   return <Provider store={store}> story();</Provider>
 }
