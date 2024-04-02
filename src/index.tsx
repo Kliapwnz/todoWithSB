f8c2f6d0ab15eae8e190b77946dd288102dd0eac
Evgeniy Klimovch <kliasanik@gmail.com>
2024-04-02 13:39:30 +0300

    customhooks for todolists

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import {AppWithRedux} from "./AppWithRedux";
 import {Provider} from "react-redux";
 import {store} from "./state/store";
+import App from "./App";
 
 
 const root = ReactDOM.createRoot(
@@ -11,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <Provider store={store}>
-        <AppWithRedux/>
+        <App/>
     </Provider>
 );
 
