f1f0f47ee3da6e3c743918a8d0d75dbbff3325d4
Evgeniy Klimovch <kliasanik@gmail.com>
2024-04-02 15:34:59 +0300

    useAddItemFrom.ts

--- src/AddItemForm/AddItemForm.tsx
+++ src/AddItemForm/AddItemForm.tsx
@@ -7,7 +7,6 @@ type AddItemFormPropsType = {
 }
 
 export const AddItemForm = memo((props: AddItemFormPropsType) => {
-   console.log("addItem")
    let [title, setTitle] = useState("")
    let [error, setError] = useState<string | null>(null)
    let addTaskHandler = () => {
