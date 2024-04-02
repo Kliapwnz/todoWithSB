fc01eb3dea229d3cf3c62b48f717fed0748e2b6c
Evgeniy Klimovch <kliasanik@gmail.com>
2024-04-02 16:00:52 +0300

    customhooks done!

--- src/AppWithRedux/hooks/useAppWithRedux.ts
+++ src/AppWithRedux/hooks/useAppWithRedux.ts
@@ -16,37 +16,31 @@ export const useAppWithRedux = () => {
    const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
 
    const dispatch = useDispatch()
-
    const changeTaskStatus = useCallback((id: string, isDone: boolean, todolistID: string) => {
       dispatch(changeTaskStatusAC(id, isDone, todolistID))
    }, [dispatch])
-
    const addTask = useCallback((title: string, todolistID: string) => {
       dispatch(addTaskAC(title, todolistID))
    }, [dispatch])
-
    const removeTask = useCallback((taskId: string, todolistID: string) => {
       dispatch(removeTaskAC(taskId, todolistID))
    }, [dispatch])
-
    const changeFilter = useCallback((value: FilterValuesType, todolistId: string) => {
       dispatch(ChangeTodolistFilterAC(todolistId, value))
    }, [dispatch])
-
    const removeTodolist = useCallback((id: string) => {
       dispatch(RemoveTodolistAC(id))
    }, [dispatch])
-
    const addTodolist = useCallback((title: string) => {
       dispatch(AddTodolistAC(title))
    }, [dispatch])
-
    const updateTask = useCallback((todolistId: string, taskId: string, title: string) => {
       dispatch(changeTaskTitleAC(taskId, title, todolistId))
    }, [dispatch])
    const updateTodolist = useCallback((todolistId: string, title: string) => {
       dispatch(ChangeTodolistTitleAC(todolistId, title))
    }, [dispatch])
+
    return {
       todolists,
       tasks,
